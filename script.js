e52f6904bfba7567069bc3b5b19c7d063ecd7bd2
Hamza Hajji <hmzhjji@gmail.com>
2017-04-01 18:45:15 +0100

    Fix typo in loop

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function init() {
 	for(let i = 0; i < 16; i++) {
-		for(let j = 0; j < 16; i++) {
+		for(let j = 0; j < 16; j++) {
 			let $div = $('<div>').addClass('pixel')
 			$('.board').append($div)
 		}
