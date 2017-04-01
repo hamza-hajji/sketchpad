a60578007651f6147984aad0baf1941a3d614f43
Hamza Hajji <hmzhjji@gmail.com>
2017-04-01 19:19:22 +0100

    Add main functionality

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 $(document).ready(function init() {
-	for(let i = 0; i < 16; i++) {
-		for(let j = 0; j < 16; j++) {
-			let $div = $('<div>').addClass('pixel')
+	// build the grid
+	let WIDTH = 700, // board size = 700x700
+		pixels = 16
+
+	for(let i = 0; i < 100; i++) {
+		for(let j = 0; j < 100; j++) {
+			let $div = $('<div>').addClass('pixel').css({
+				width: WIDTH / pixels + 'px',
+				height: WIDTH / pixels + 'px'
+			})
 			$('.board').append($div)
 		}
 	}
-})
+
+	// paint the div(pixel) on hover
+	$('.pixel').hover(function paint(e) {
+		$(this).addClass('painted')
+	})
+}) 
